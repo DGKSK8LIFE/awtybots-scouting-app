47e1883d0066cabcee3a5d1f82e9a3802e8d0236
Tarek Ali <tarekali15@outlook.com>
2020-08-18 16:21:05 -0500

    db.sql

--- backend/db.sql
+++ backend/db.sql
@@ -1,2 +1,15 @@
 CREATE DATABASE Scout_DB;
 USE Scout_DB;
+
+CREATE TABLE recording (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    team VARCHAR(36) PRIMARY KEY,
+    starting_location UNSIGNED TINYINT NOT NULL,
+    moved BOOLEAN NOT NULL,
+    inner_port SMALLINT NOT NULL,
+    outer_port SMALLINT NOT NULL,
+    lower_port SMALLINT NOT NULL,
+    control_panel_stage_two BOOLEAN,
+    control_panel_stage_three BOOLEAN,
+    hang_time TINYINT NOT NULL
+);
