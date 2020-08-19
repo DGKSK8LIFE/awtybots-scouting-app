f0c524443cde00dcdf07628e6dc041378d72bf7c
Tarek Ali <tarekali15@outlook.com>
2020-08-19 15:04:22 -0500

    handler.go

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,8 @@ module scouting-app/backend
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/jmoiron/sqlx v1.2.0
+)
--- backend/handler/handler.go
+++ backend/handler/handler.go
@@ -1,2 +1,14 @@
 package handler
 
+import (
+	"scouting-app/backend/dbutils"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Recordings returns json serialization of an array of Recordings
+func Recordings(c *gin.Context) {
+	recordings := []dbutils.Recording{}
+	dbutils.SelectRecordings(recordings)
+	c.JSON(200, recordings)
+}
