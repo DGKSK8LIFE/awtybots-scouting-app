b9fbc388e81a6be8ffeb64fdbd8a0bc48d75959e
Tarek Ali <tarekali15@outlook.com>
2020-08-19 14:53:37 -0500

    main.go more code m8s

--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,16 @@
 package main
 
 import (
+	"scouting-app/backend/dbutils"
+	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	dbutils.Open()
+}
+
+func main() {
+	r := gin.Default()
+	// r.GET("/api/recordings",  ) 
+	r.Run()
+}
