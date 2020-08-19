7a4ff5441e09f06f29d796487456a7d232bfa0ea
Tarek Ali <tarekali15@outlook.com>
2020-08-19 09:40:43 -0500

    db work

--- backend/main.ts
+++ backend/main.ts
@@ -2,3 +2,16 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express();
 
+const db = mysql.createConnection({
+  host: 'mysql',
+  user: 'root',
+  password: 'root',
+  database: 'Scout_DB'
+});
+
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('db has successfully connected');
+})
