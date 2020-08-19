9c0abe391d9a33d045c7a58b4ddc95fea82599f6
Tarek Ali <tarekali15@outlook.com>
2020-08-19 09:20:33 -0500

    new deps (db.ts still unstable)

---
+++ backend/utils/db.ts
@@ -0,0 +1,21 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
+
+@Entity()
+export class recording {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+  
+  @Column
+  team: number;
+
+  @Column
+  starting_location: number;
+
+  @Column
+  moved: boolean;
+
+  @Column
+  inner_port: 
+    
+}
