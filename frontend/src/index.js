5a170a70fa1d232164f1e8ba08ad54b4374c5976
Tarek Ali <tarekali15@outlook.com>
2020-08-21 23:18:50 -0500

    moved stuff, changed theme

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import Home from './pages/Home';
 import * as serviceWorker from './serviceWorker';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
