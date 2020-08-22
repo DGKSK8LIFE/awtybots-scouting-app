875bae9157ecc1da4082f7ea7a4058620548f075
Tarek Ali <tarekali15@outlook.com>
2020-08-22 10:55:30 -0500

    removed manual css

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -5,11 +5,9 @@ import '../styles/dist/navbar.css';
 export default function NavBar() {
     return (
       <div className="navbar">
-        <div className="hyperlinks">
           <Link to="">View Graphs </Link>
           <Link to="">Create Recordings </Link>
           <Link to="">View Recordings </Link>
-        </div>
       </div>
     );
 }
