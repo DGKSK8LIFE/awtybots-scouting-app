388bf8d1e4c53b4ed37cd80f28034b85fb3d67a3
Tarek Ali <tarekali15@outlook.com>
2020-08-22 11:14:59 -0500

    MY BRADAS: TAILWINDCSS IS WORKING. WE WILL SUCCEED

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function NavBar() {
     return (
-      <div className="navbar">
+      <div className="bg-white rounded-lg p-6 text-purple-500">
           <Link to="">View Graphs </Link>
           <Link to="">Create Recordings </Link>
           <Link to="">View Recordings </Link>
