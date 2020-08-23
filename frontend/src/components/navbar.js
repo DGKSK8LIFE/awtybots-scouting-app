2b270d019191665e97744c0a70d880743c5b333c
Tarek Ali <tarekali15@outlook.com>
2020-08-23 08:27:06 -0500

    no more tailwind stuff

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Navbar, Nav, Form } from 'react-bootstrap';
 
 export default function NavBar() {
     return (
-      <div className="">
-          <Link to="">View Graphs </Link>
-          <Link to="">Create Recordings </Link>
-          <Link to="">View Recordings </Link>
-      </div>
+      <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-info">Search</Button>
+    </Form>
+  </Navbar>
     );
 }
