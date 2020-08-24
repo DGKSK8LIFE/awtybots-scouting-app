8fd70a37e834325f131e2c219bb2924780e2f4c0
Tarek Ali <tarekali15@outlook.com>
2020-08-24 14:00:07 -0500

    added new routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/index.css';
 import Home from './pages/Home';
+import Graphs from './pages/Graphs';
+import Create from './pages/Create';
 import * as serviceWorker from './serviceWorker';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,6 +14,12 @@ ReactDOM.render(
       <Switch>
         <Route exact path='/'>
           <Home />
+        </Route>
+        <Route exact path='/graphs'>
+         <Graphs /> 
+        </Route>
+        <Route exact path='/create-recording'>
+          <Create />
         </Route>
       </Switch>
     </BrowserRouter>
---
+++ frontend/src/pages/dist/Graphs.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = Graphs;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function Graphs() {}
