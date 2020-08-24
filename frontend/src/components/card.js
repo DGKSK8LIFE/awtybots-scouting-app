de13e01bfbe05f9b3abc306704a849c3a4f77b3b
Tarek Ali <tarekali15@outlook.com>
2020-08-24 13:27:46 -0500

    completed card.js (changes will probably be made soon though

--- frontend/src/components/card.js
+++ frontend/src/components/card.js
@@ -4,9 +4,39 @@ import { Card, Button } from 'react-bootstrap';
 function Card(props) {
     return (
         <Card>
-            <Card.Header as="h5">{props.teamName}</Card.Header>
+            <Card.Header as="h5">{props.team}</Card.Header>
             <Card.Body>
                <ul>
+                   <li>
+                      ID: {props.id}  
+                   </li>
+                   <li>
+                      Starting Location: {props.startingLocation}
+                   </li>
+                   <li>
+                      Moved?: {props.moved}
+                   </li>
+                   <li>
+                      Inner Port Shots: {props.innerPort}
+                   </li>
+                   <li>
+                      Outer Port Shots: {props.outerPort}
+                   </li>
+                   <li>
+                       Lower Port Shots: {props.lowerPort}
+                   </li>
+                   <li>
+                       Control Panel Stage Two?: {props.controlPanelStageTwo}
+                   </li>
+                   <li>
+                       Control Panel Stage Three?: {props.controlPanelStageThree}
+                   </li>
+                   <li>
+                        Hangtime: {props.hangTime}                       
+                   </li>
+                   <li>
+                       Time: {props.time}
+                   </li>
                </ul> 
             </Card.Body>
         </Card>
