33cf9b4719988142df1b8b4206096ab9db052785
Tarek Ali <tarekali15@outlook.com>
2020-08-24 12:31:14 -0500

    navbar styles

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import '../assets/dist/navbar.css';
 
 export default function NavBar() {
     return (
+      <div className="scoutingnavbar">
       <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Navbar.Brand href="/">View Recordings</Navbar.Brand>
     <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link href="/graphs">View Graphs</Nav.Link>
+      <Nav.Link href="/create-recording">Create Recordings</Nav.Link>
     </Nav>
     <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <FormControl type="text" placeholder="Search Recordings" className="mr-sm-2" />
       <Button variant="outline-info">Search</Button>
     </Form>
   </Navbar>
+  </div>
     );
 }
