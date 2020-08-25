61070ee6870b1025483d6fd19fecfdf868db9e09
Tarek Ali <tarekali15@outlook.com>
2020-08-25 08:21:01 -0500

    changed syntax of Card() to make invocation easier

--- frontend/src/components/card.js
+++ frontend/src/components/card.js
@@ -4,38 +4,38 @@ import { Card, Button } from 'react-bootstrap';
 export default function Card(props) {
     return (
         <Card>
-            <Card.Header as="h5">{props.team}</Card.Header>
+            <Card.Header as="h5">{props.obj.team}</Card.Header>
             <Card.Body>
                <ul>
                    <li>
-                      ID: {props.id}  
+                      ID: {props.obj.id}  
                    </li>
                    <li>
-                      Starting Location: {props.startingLocation}
+                      Starting Location: {props.obj.startingLocation}
                    </li>
                    <li>
-                      Moved?: {props.moved}
+                      Moved?: {props.obj.moved}
                    </li>
                    <li>
-                      Inner Port Shots: {props.innerPort}
+                      Inner Port Shots: {props.obj.innerPort}
                    </li>
                    <li>
-                      Outer Port Shots: {props.outerPort}
+                      Outer Port Shots: {props.obj.outerPort}
                    </li>
                    <li>
-                       Lower Port Shots: {props.lowerPort}
+                       Lower Port Shots: {props.obj.lowerPort}
                    </li>
                    <li>
-                       Control Panel Stage Two?: {props.controlPanelStageTwo}
+                       Control Panel Stage Two?: {props.obj.controlPanelStageTwo}
                    </li>
                    <li>
-                       Control Panel Stage Three?: {props.controlPanelStageThree}
+                       Control Panel Stage Three?: {props.obj.controlPanelStageThree}
                    </li>
                    <li>
-                       Hangtime: {props.hangTime}                       
+                       Hangtime: {props.obj.hangTime}                       
                    </li>
                    <li>
-                       Time: {props.time}
+                       Time: {props.obj.time}
                    </li>
                </ul> 
             </Card.Body>
