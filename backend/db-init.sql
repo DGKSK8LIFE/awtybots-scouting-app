c1b3565e4de93322346617def5fcbe1d3f731585
Tarek Ali <tarekali15@outlook.com>
2020-08-25 20:56:59 -0500

    fixed error in mysql syntax

--- backend/db-init.sql
+++ backend/db-init.sql
@@ -4,7 +4,7 @@ USE Scout_DB;
 CREATE TABLE Recording (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     team INT NOT NULL,
-    starting_location UNSIGNED TINYINT NOT NULL,
+    starting_location TINYINT NOT NULL,
     moved BOOLEAN NOT NULL,
     inner_port SMALLINT NOT NULL,
     outer_port SMALLINT NOT NULL,
