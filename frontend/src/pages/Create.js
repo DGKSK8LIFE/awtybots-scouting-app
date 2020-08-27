eb01f29d6cabc7194db8d7d370ac3d2920ba3357
Tarek Ali <tarekali15@outlook.com>
2020-08-27 11:28:02 -0500

    added more field items

--- frontend/src/pages/Create.js
+++ frontend/src/pages/Create.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form } from "react-bootstrap";
+import { Form, Col, Row } from "react-bootstrap";
 import NavBar from "../components/navbar";
 
 export default function Create() {
@@ -10,6 +10,36 @@ export default function Create() {
       <Form>
         <Form.Group controlId="formGroupTeam">
           <Form.Control type="number" placeholder="Enter team" />
+        </Form.Group>
+        <Form.Group controlId="formGroupStartingLocation" as={Row}>
+          <Form.Label as="legend" column sm={2}>
+            Starting Location
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Check 
+              label="left"
+            />
+            <Form.Check 
+              label="center"
+            />
+            <Form.Check 
+              label="right"
+            />
+          </Col>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label as="legend" column sm={2}>
+            Moved?
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Check 
+            label="yes"
+            />
+            <Form.Check 
+              label="no"           
+            />
+
+          </Col>
         </Form.Group>
         <Form.Group controlId="formGroupInnerPort">
           <Form.Control type="number" placeholder="Enter inner port score" />
