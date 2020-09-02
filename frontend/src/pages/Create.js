f31556a982e812a24023073778a2c293f34a77a9
Tarek Ali <tarekali15@outlook.com>
2020-09-02 09:40:12 -0500

    stylizing the create page

--- frontend/src/pages/Create.js
+++ frontend/src/pages/Create.js
@@ -1,78 +1,92 @@
 import React from "react";
 import { Form, Col, Row, Button } from "react-bootstrap";
 import NavBar from "../components/navbar";
-import axios from 'axios';
+import axios from "axios";
 
-const submitForm = () => {
-
-}
+const submitForm = () => {};
 
 export default function Create() {
   return (
     <React.Fragment>
       <NavBar></NavBar>
       <br></br>
-      <h1>Create a Recording</h1>
-      <br></br>
-      <Form>
-        <Form.Group controlId="formGroupTeam">
-          <Form.Control type="number" placeholder="Enter team" required/>
-        </Form.Group>
-        <Form.Group controlId="formGroupStartingLocation">
-          <Form.Label as="legend" column sm={2}>
-            Starting Location
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Check label="left" />
-            <Form.Check label="center" />
-            <Form.Check label="right" />
-          </Col>
-        </Form.Group>
-        <Form.Group controlId="formGroupMoved?">
-          <Form.Label as="legend" column sm={2}>
-            Moved?
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Check label="yes" />
-            <Form.Check label="no" />
-          </Col>
-        </Form.Group>
-        <Form.Group controlId="formGroupInnerPort">
-          <Form.Control type="number" placeholder="Enter inner port score" required/>
-        </Form.Group>
-        <Form.Group controlId="formGroupOuterPort">
-          <Form.Control type="number" placeholder="Enter outer port score" required/>
-        </Form.Group>
-        <Form.Group controlId="formGroupLowerPort">
-          <Form.Control type="number" placeholder="Enter lower port score" required/>
-        </Form.Group>
-        <Form.Group controlId="formGroupControlPanelStageTwo">
-          <Form.Label as="legend" column sm={2}>
-            Control Panel Stage Two?
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Check label="yes" />
-            <Form.Check label="no" />
-          </Col>
-        </Form.Group>
-        <Form.Group controlId="formGroupControlPanelStageThree">
-          <Form.Label as="legend" column sm={2}>
-            Control Panel Stage Three
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Check label="yes" />
-            <Form.Check label="no" />
-          </Col>
-        </Form.Group>
-        <Form.Group controlId="formGroupHangtime">
-          <Form.Control type="number" placeholder="Enter hangtime" />
-        </Form.Group>
-        <Form.Group as={Row}>
-          <Col sm={{ span: 10, offset: 2 }}>
-            <Button type="submit" onclick={submitForm()}>Create Recording</Button>
-          </Col>
-        </Form.Group>
-      </Form>
+      <div className="create">
+        <h1>Create a Recording</h1>
+        <br></br>
+        <Form>
+          <Form.Group controlId="formGroupTeam">
+            <Form.Control type="number" placeholder="Enter team" required />
+          </Form.Group>
+          <Form.Group controlId="formGroupStartingLocation">
+            <Form.Label as="legend" column sm={2}>
+              Starting Location
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Check label="left" />
+              <Form.Check label="center" />
+              <Form.Check label="right" />
+            </Col>
+          </Form.Group>
+          <Form.Group controlId="formGroupMoved?">
+            <Form.Label as="legend" column sm={2}>
+              Moved?
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Check label="yes" />
+              <Form.Check label="no" />
+            </Col>
+          </Form.Group>
+          <Form.Group controlId="formGroupInnerPort">
+            <Form.Control
+              type="number"
+              placeholder="Enter inner port score"
+              required
+            />
+          </Form.Group>
+          <Form.Group controlId="formGroupOuterPort">
+            <Form.Control
+              type="number"
+              placeholder="Enter outer port score"
+              required
+            />
+          </Form.Group>
+          <Form.Group controlId="formGroupLowerPort">
+            <Form.Control
+              type="number"
+              placeholder="Enter lower port score"
+              required
+            />
+          </Form.Group>
+          <Form.Group> controlId="formGroupControlPanelStageTwo">
+            <Form.Label as="legend" column sm={2}>
+              Control Panel Stage Two?
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Check label="yes" />
+              <Form.Check label="no" />
+            </Col>
+          </Form.Group>
+          <Form.Group controlId="formGroupControlPanelStageThree">
+            <Form.Label as="legend" column sm={2}>
+              Control Panel Stage Three
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Check label="yes" />
+              <Form.Check label="no" />
+            </Col>
+          </Form.Group>
+          <Form.Group controlId="formGroupHangtime">
+            <Form.Control type="number" placeholder="Enter hangtime" />
+          </Form.Group>
+          <Form.Group as={Row}>
+            <Col sm={{ span: 10, offset: 2 }}>
+              <Button type="submit" onclick={submitForm()}>
+                Create Recording
+              </Button>
+            </Col>
+          </Form.Group>
+        </Form>
+      </div>
     </React.Fragment>
   );
 }
