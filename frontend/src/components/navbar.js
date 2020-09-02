9e754c9363be979cc5a013e71f5aacef13d4b5aa
Tarek Ali <tarekali15@outlook.com>
2020-09-02 09:25:05 -0500

    added external css stye overrides; stylizing navbar a bit more as you all can see

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,23 +1,28 @@
 import React from "react";
 import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import "../assets/navbar.css";
 
 // Navbar is the navbar component for the app
 export default function NavBar() {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="/">View Recordings</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="/graphs">View Graphs</Nav.Link>
-        <Nav.Link href="/create-recording">Create Recordings</Nav.Link>
-      </Nav>
-      <Form inline>
-        <FormControl
-          type="text"
-          placeholder="Search Recordings"
-          className="mr-sm-2"
-        />
-        <Button variant="outline-info">Search</Button>
-      </Form>
-    </Navbar>
+    <div className="navbar">
+      <Navbar>
+        <Navbar.Brand href="/">View Recordings</Navbar.Brand>
+        <div className="text">
+          <Nav className="mr-auto">
+            <Nav.Link href="/graphs">View Graphs</Nav.Link>
+            <Nav.Link href="/create-recording">Create Recordings</Nav.Link>
+          </Nav>
+        </div>
+        <Form inline>
+          <FormControl
+            type="text"
+            placeholder="Search Recordings"
+            className="mr-sm-2"
+          />
+          <Button variant="dark">Search</Button>
+        </Form>
+      </Navbar>
+    </div>
   );
 }
